0306c0ab3b6a363fdf6d0de52933285937d45743
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-03-02 18:14:42 -1000

    greyscale slider

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
+let slider1; 
+
 function setup() {
-  
+  createCanvas(windowWidth, windowHeight); 
+  // greyscale slider
+  slider1 = createSlider(0, 255);
 }
 
 function draw() {
-  
+  // changes background color
+  background(slider1.value());
 }
