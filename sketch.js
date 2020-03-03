062854da346aaad91d14fd283fc90d3ddf7c4841
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-03-02 18:23:24 -1000

    individual rgb sliders

--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
 let slider1; 
 
 function setup() {
+  colorMode(RGB);
   createCanvas(windowWidth, windowHeight); 
   // returns value 0-255
-  slider1 = createSlider(0, 255);
+  rSlider = createSlider(0, 255);
+  gSlider = createSlider(0, 255);
+  bSlider = createSlider(0, 255);
 }
 
 function draw() {
   // changes background color
-  // greyscale - changes each RGB value equally 
-  background(slider1.value(), slider1.value(), slider1.value());
+  // individual slider for each input value 
+  background(rSlider.value(), gSlider.value(), bSlider.value());
+
+  // red (255, 0, 0)
+  // green(0, 255, 0)
+  // yellow(255, 255, 0)
+  // brown(125, 125, 100)
 }
